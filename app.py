f216913369d1ef0125d47ed88a8f6f54f7dc9926
safrinafanfei <feif1021@gmail.com>
2016-07-23 19:43:50 -0400

    new push

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+import csv
 from flask import Flask
 from flask import render_template
 app = Flask(__name__) 
+
+def get_csv():
+	csv_path = './static/la-riots-deaths.csv'
 
 @app.route("/")
 def index():
